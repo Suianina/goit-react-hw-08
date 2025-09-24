6668eaa1f14330930a96963f8c598a4343911156
Zhanna Suianina <cezyby3@gmail.com>
2025-09-24 16:38:14 +0300

    feat(contact-list): add empty state and stabilize layout on delete

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,13 +1,20 @@
-import css from './ContactList.module.css'
-import Contact from '../Contact/Contact'
+import css from "./ContactList.module.css";
+import Contact from "../Contact/Contact";
 
-const ContactList = ({contacts}) => {
-    return(
-        <ul className={css.list}>
-            {contacts.map(contact => (
-                <Contact key={contact.id} contact={contact}/>
-            ))}
-        </ul>
-    )
-}
-export default ContactList
+const ContactList = ({ contacts = [] }) => {
+  if (!contacts.length) {
+    return <p className={css.empty}>No contacts yet</p>;
+  }
+
+  return (
+    <ul className={css.list}>
+      {contacts.map((c) => (
+        <li key={c.id} className={css.item}>
+          <Contact contact={c} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ContactList;
